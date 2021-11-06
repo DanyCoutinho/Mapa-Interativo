87247a4349a835cbf709cda784208bc6f001fc24
Dany Coutinho <62888708+DanyCoutinho@users.noreply.github.com>
2021-11-06 01:20:26 +0000

    Initial commit

---
+++ js/jsmap.js
@@ -0,0 +1,137 @@
+$(document).ready(function(){
+    $(".marker-00").hover(function(){
+    $(".txt-00").toggle();
+});
+});
+
+$(document).ready(function(){
+$(".marker-01").hover(function(){
+$(".txt-01").toggle();
+});
+});
+
+$(document).ready(function(){
+    $(".marker-02").hover(function(){
+    $(".txt-02").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-03").hover(function(){
+    $(".txt-03").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-04").hover(function(){
+    $(".txt-04").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-05").hover(function(){
+    $(".txt-05").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-06").hover(function(){
+    $(".txt-06").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-07").hover(function(){
+    $(".txt-07").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-08").hover(function(){
+    $(".txt-08").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-09").hover(function(){
+    $(".txt-09").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-10").hover(function(){
+    $(".txt-10").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-11").hover(function(){
+    $(".txt-11").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-12").hover(function(){
+    $(".txt-12").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-13").hover(function(){
+    $(".txt-13").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-14").hover(function(){
+    $(".txt-14").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-15").hover(function(){
+    $(".txt-15").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-16").hover(function(){
+    $(".txt-16").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-17").hover(function(){
+    $(".txt-17").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-18").hover(function(){
+    $(".txt-18").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-19").hover(function(){
+    $(".txt-19").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-20").hover(function(){
+    $(".txt-20").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-21").hover(function(){
+    $(".txt-21").toggle();
+    });
+    });
+
+$(document).ready(function(){
+    $(".marker-22").hover(function(){
+    $(".txt-22").toggle();
+    });
+    });
